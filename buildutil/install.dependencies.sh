fb03f0e62f95d343edc1db621ae884e0db695f2e
Ryan Thomas <ryan@smashenter.com>
2025-09-02 12:30:25 -0500

    Use sudo for cp & chmod

--- buildutil/install.dependencies.sh
+++ buildutil/install.dependencies.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/dlemstra/Magick.NET.BuildDependencies/releases/download/build-binaries-2025-08-30/ghostscript-10.0.0-linux-x86_64.tgz
 tar zxvf ghostscript-10.0.0-linux-x86_64.tgz
-cp ghostscript-10.0.0-linux-x86_64/gs-1000-linux-x86_64 /usr/bin/gs
-chmod 755 /usr/bin/gs
+sudo cp ghostscript-10.0.0-linux-x86_64/gs-1000-linux-x86_64 /usr/bin/gs
+sudo chmod 755 /usr/bin/gs
