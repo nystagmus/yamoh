d8e8ad0f09548f5f215fef3a0821457e73f74d92
Ryan Thomas <ryan@smashenter.com>
2025-09-02 12:33:50 -0500

    Trying the full dependencies script from Magick.net

--- buildutil/install.dependencies.sh
+++ buildutil/install.dependencies.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+set -e
+
+apt-get update
+
+apt-get install ffmpeg fontconfig libgomp1 unzip -y
+
+git clone https://github.com/ImageMagick/msttcorefonts msttcorefonts
+cd msttcorefonts
+. ./install.sh
+
+fc-cache
+
 wget https://github.com/dlemstra/Magick.NET.BuildDependencies/releases/download/build-binaries-2025-08-30/ghostscript-10.0.0-linux-x86_64.tgz
 tar zxvf ghostscript-10.0.0-linux-x86_64.tgz
 sudo cp ghostscript-10.0.0-linux-x86_64/gs-1000-linux-x86_64 /usr/bin/gs
